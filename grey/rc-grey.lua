a372132dec3184b30290bc0f3f63f23387fc1b98
Greyowl <solaceiiix@gmail.com>
2022-11-12 01:55:01 -0800

    cleanup: lesser used layouts

--- grey/rc-grey.lua
+++ grey/rc-grey.lua
@@ -47,10 +47,8 @@ bling.widget.window_switcher.enable {
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.tile,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.tile.right,
-    awful.layout.suit.tile.left,
-	awful.layout.suit.fair,
+    awful.layout.suit.magnifier,
+	  awful.layout.suit.fair,
     awful.layout.suit.max,
 }
 
