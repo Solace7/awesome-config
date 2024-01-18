8df32d81c66a05f44ccc457c19ba3efa3bd904eb
Greyowl <solaceiiix@gmail.com>
2024-01-17 21:01:37 -0800

    ref(testing): Xephyr startup script.
    - Exits when config is closed

--- scripts/run-with-xephyr.sh
+++ scripts/run-with-xephyr.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
-Xephyr :3 -ac -screen 1920x1080 &
+Xephyr :3 -ac -screen 1280x720 & #1920x1080 &
 XEPHYR_PID=$!
 sleep 0.5
 
-DISPLAY=:3 awesome -c rc-devel.lua
-kill ${XEPHYR_PID}
+DISPLAY=:3 awesome -c $HOME/.config/awesome/rc-devel.lua &
+instance=$!
+
+while inotifywait -r -e close_write ~/.config/awesome/rc-devel.lua; do
+  kill ${XEPHYR_PID}
+done
