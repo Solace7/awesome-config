fd93480ee8a78d9fb6baa473d8467b082c990764
Greyowl <solaceiiix@gmail.com>
2024-03-20 16:04:30 -0700

    chore: Use env scriptsdir var

--- modules/signals.lua
+++ modules/signals.lua
@@ -57,7 +57,7 @@ function signals:listen(args)
         end 
     end)
     --Screen handling
-    screen.connect_signal("list", function() awful.spawn(scriptsdir .. "display_setup.sh") end )
+    screen.connect_signal("list", function() awful.spawn(env.scriptsdir .. "display_setup.sh") end )
    
 end
 
